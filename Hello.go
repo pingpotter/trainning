ac202edd5918660590165022168c6e0fff1c7f2c
chawapoltnis <chawapol.it@tnis.com>
2017-10-12 16:26:41 +0700

    Tangmo put

--- Hello.go
+++ Hello.go
@@ -8,6 +8,7 @@ func main() {
 	fmt.Println("Hello2, 世界")
 	fmt.Println("Hello3")
 	fmt.Println("Petch777")
-	fmt.Println("P'new")
+	fmt.Println("P'new is a funny person.")
 	fmt.Println("Petch777")
+	fmt.Println("Tangmo is very pretty!")
 }
