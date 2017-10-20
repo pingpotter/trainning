685ce6a2254a1b6f62c3dc583d77a0b33914be51
chawapoltnis <chawapol.it@tnis.com>
2017-10-20 16:34:23 +0700

    wallet account project

--- wallet/main.go
+++ wallet/main.go
@@ -1,2 +1,26 @@
 package wallet
-//ping
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type WalletX struct {
+	Id    	  int16   `json:"id"`
+	Citizen   int16   `json:"citizen"`
+	Fullname  string `json:"name"`
+	Opendate   time.Time   `json:"time"`
+	Balance   int16	  `json:"id"`
+}
+
+var appName = "wallet service"
+
+func main() {
+
+	fmt.Printf("Starting %v\n", appName)
+	router := NewRouter()
+	log.Fatal(http.ListenAndServe(":6677", router))
+
+}
--- wallet/routes.go
+++ wallet/routes.go
@@ -1,2 +1,33 @@
 package wallet
-//ping
+
+import (
+	"net/http"
+	"mux"
+)
+// Defines a single route, e.g. a human readable name, HTTP method and the
+// pattern the function that will execute when the route is called.
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+// Defines the type Routes which is just an array (slice) of Route structs.
+type Routes []Route
+
+// Initialize our routes
+var routes = Routes{
+
+	Route{
+		"GetWallet",                                     // Name
+		"GET",                                            // HTTP method
+		"/v1/accounts/{walletId}",                          // Route pattern
+		func(w http.ResponseWriter, r *http.Request) {
+			vars := mux.Vars(r)
+			w_id := vars["walletId"]
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.Write([]byte("{\"result\":\"OK\"}"+w_id))
+		},
+	},
+}
